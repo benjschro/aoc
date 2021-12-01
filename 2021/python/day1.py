75daa3fc610d53583adbe2ae1475114bc1055823
Benjamin J. Schroeder <benjschro@gmail.com>
2021-12-01 00:16:30 -0500

    Day 1 2021

---
+++ 2021/python/day1.py
@@ -0,0 +1,50 @@
+with open('../input/day1.txt') as file:
+    myList = [int(line.strip()) for line in file]
+
+# increases = 0
+
+# first = True
+# last = None
+# for item in myList:
+#     if first is True:
+#         first = False
+#     else:
+#         if item > last:
+#             increases += 1
+#     last = item
+
+last1 = None
+last2 = None
+last3 = None
+lastSum = None
+
+partOne = 0
+partTwo = 0
+
+for item in myList:
+    if last1 is None:
+        last1 = item
+    elif last2 is None:
+        last2 = item
+    elif last3 is None:
+        last3 = item
+        lastSum = last1 + last2 + last3
+    else:
+        last1 = last2
+        last2 = last3
+        last3 = item
+        newSum = last1 + last2 + last3
+        if newSum > lastSum:
+            partTwo += 1
+        lastSum = newSum
+    
+    if last2 is not None:
+        if last2 > last1:
+            partOne += 1
+
+print("partOne = " + str(partOne))
+print("partTwo = " + str(partTwo))
+
+# $ python3 day1.py
+# partOne = 1316
+# partTwo = 1344
