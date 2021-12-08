56c39cc49f25fa70b8fc76ede286d94fff5cd91a
Benjamin J. Schroeder <benjschro@gmail.com>
2021-12-08 14:48:18 -0500

    Cleanup

--- 2021/python/day8.py
+++ 2021/python/day8.py
@@ -1,102 +1,67 @@
 with open('../input/day8.txt') as file:
     myList = [line.strip() for line in file]
 
-digitDict = {}
+discoveredSignals = {}
+
+numSegmentCounts = {0: 6, 1: 2, 2: 5, 3: 5, 4: 4, 5: 5, 6: 6, 7: 3, 8: 7, 9: 6}
 
 def addToDict(items):
     for item in items:
         if len(item) == 2:
-            digitDict[1] = item
+            discoveredSignals[1] = item
         elif len(item) == 4:
-            digitDict[4] = item
+            discoveredSignals[4] = item
         elif len(item) == 3:
-            digitDict[7] = item
+            discoveredSignals[7] = item
         elif len(item) == 7:
-            digitDict[8] = item
+            discoveredSignals[8] = item
+
+def solveNumber(numToSolve, refNum, signalPatterns, expectedMatches):
+    for signalPattern in signalPatterns:
+        if signalPattern in discoveredSignals.values():
+            continue
+        elif len(signalPattern) == numSegmentCounts[numToSolve]:
+            count = 0
+            for letter in discoveredSignals[refNum]:
+                if letter in signalPattern:
+                    count += 1
+            if count == expectedMatches:
+                return signalPattern
 
 partOneSum = 0
 partTwoSum = 0
 for line in myList:
-    digitDict.clear()
-    wires = line.split('|')[0].strip().split(' ')
-    addToDict(wires)
+    discoveredSignals.clear()
+
+    signalPatterns = line.split('|')[0].strip().split(' ')
+    addToDict(signalPatterns)
+
     segments = line.split('|')[1].strip().split(' ')
-    addToDict(segments)
 
-    letterCount = {}
-    for letter in 'abcdefg':
-        for wire in wires:
-            if letter in wire:
-                if letter in letterCount:
-                    letterCount[letter] += 1
-                else:
-                    letterCount[letter] = 1
-    
-    # 1, 4, 7, 8 are solved
-    # Solve for 2
-    for wire in wires:
-        if len(wire) == 5: # 2, 3, 5
-            count = 0
-            for letter in digitDict[4]:
-                if letter in wire:
-                    count += 1
-            if count == 2:
-                digitDict[2] = wire
+    discoveredSignals[2] = solveNumber(numToSolve=2, refNum=4, signalPatterns=signalPatterns, expectedMatches=2) # 1,2,4,7,8 are solved
+    discoveredSignals[3] = solveNumber(numToSolve=3, refNum=1, signalPatterns=signalPatterns, expectedMatches=2) # 1,2,3,4,7,8 are solved
+    discoveredSignals[5] = solveNumber(numToSolve=5, refNum=1, signalPatterns=signalPatterns, expectedMatches=1) # 1,2,3,4,5,7,8 are solved
+    discoveredSignals[6] = solveNumber(numToSolve=6, refNum=7, signalPatterns=signalPatterns, expectedMatches=2) # 1,2,3,4,5,6,7,8 are solved
+    discoveredSignals[0] = solveNumber(numToSolve=0, refNum=3, signalPatterns=signalPatterns, expectedMatches=4) # 0,1,2,3,4,5,6,7,8 are solved
+    discoveredSignals[9] = solveNumber(numToSolve=9, refNum=3, signalPatterns=signalPatterns, expectedMatches=5) # All digits solved
+ 
+    solvedOutput = ''
 
-    # 1, 2, 4, 7, 8 are solved
-    # Solve for 3 and 5
-    for wire in wires:
-        if len(wire) == 5 and digitDict[2] != wire: # 3, 5
-            count = 0
-            for letter in digitDict[1]:
-                if letter in wire:
-                    count += 1
-            if count == 2:
-                digitDict[3] = wire
-            elif count == 1:
-                digitDict[5] = wire
-
-    # 1, 2, 3, 4, 5, 7, 8 are solved
-    # Solve for 6
-    for wire in wires:
-        if len(wire) == 6: # 0, 6, 9
-            count = 0
-            for letter in digitDict[7]:
-                if letter in wire:
-                    count += 1
-            if count == 2:
-                digitDict[6] = wire
-
-    # 1, 2, 3, 4, 5, 6, 7, 8 are solved
-    # Solve for 0 and 9
-    for wire in wires:
-        if len(wire) == 6 and digitDict[6] != wire: # 0, 9
-            count = 0
-            for letter in digitDict[3]:
-                if letter in wire:
-                    count += 1
-            if count == 5:
-                digitDict[9] = wire
-            elif count == 4:
-                digitDict[0] = wire
-
-    outputValue = ''
-
-    for item in digitDict:
-        digitDict[item] = ''.join(sorted(digitDict[item]))
+    for item in discoveredSignals:
+        discoveredSignals[item] = ''.join(sorted(discoveredSignals[item]))
 
     for i in range(0, len(segments)):
         segments[i] = ''.join(sorted(segments[i]))
     
     for segment in segments:
-        for number, letters in digitDict.items():
+        for number, letters in discoveredSignals.items():
             if segment == letters:
-                outputValue += str(number)
-                if str(number) in '1478':
+                solvedOutput += str(number)
+                if number in [1,4,7,8]:
                     partOneSum += 1
                 break
 
-    partTwoSum += int(outputValue)
+    partTwoSum += int(solvedOutput)
 
 print('partOne = ' + str(partOneSum))
 print('partTwo = ' + str(partTwoSum))
